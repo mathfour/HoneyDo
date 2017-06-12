5d8f6295fac4101cd2384d90415dc84bb37f7219
Bon Crowder <SilverDash@Bons-MacBook-Pro.local>
2017-06-11 19:18:00 -0500

    JawDB code inserted into connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,19 @@
 
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+
+
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "",
     database: "burger_muncher"
 });
+}
 
 // Initiate MySQL Connection.
 connection.connect(function (err) {
